f1233a2f42b44ceb6369c87013f9304dca987aaa
sylverpyro <sylverpyro@users.noreply.github.com>
2023-10-27 09:59:28 -0500

    progress on python conversion

--- nssg.py
+++ nssg.py
@@ -12,16 +12,32 @@ def defaults():
     # Support for arbitrary storefront compatdata folders coming in 0.5
     # support for sdcard installed storefronts coming in 0.5
 
+    ## Epic Games Launcher paths
+    global egs
 
-    # Epic Games Launcher paths
-    egl_storefront_name = "Epic Games Store"
+    #egl_storefront_name = "Epic Games Store"
+    egs.name="Epic Games Store"
+
     # To launch the storefront in the EGL it actualy needs a special launch option
-    egl_storefront_launch_opts='-com.epicgames'
-    egl_comp_dir="$compdata_dir/EpicGamesLauncher"
-    egl_pfx="$egl_comp_dir/pfx"
-    egl_start_dir="$egl_pfx/drive_c/Program Files (x86)/Epic Games"
-    egl_exe="$egl_pfx/drive_c/Program Files (x86)/Epic Games/Launcher/Portal/Binaries/Win32/EpicGamesLauncher.exe"
-    egl_games_dir="$egl_pfx/drive_c/Program Files/Epic Games"
+    egs.launch_opts = '-com.epicgames'
+    #egl_storefront_launch_opts='-com.epicgames'
+
+    egs.compat_path = os.path.join(steam_paths.compatdata_dir,"EpicGamesLauncher")
+    #egl_comp_dir="$compdata_dir/EpicGamesLauncher"
+
+    egs.wine_pfx=os.path.join(egs.compat_path,'pfx')
+    #egl_pfx="$egl_comp_dir/pfx"
+
+    egs.start_dir = os.path.join(egs.wine_pfx,'drive_c/Program Files (x86)/Epic Games')
+    #egl_start_dir="$egl_pfx/drive_c/Program Files (x86)/Epic Games"
+
+    egs.exe_name = 'EpicGamesLauncher.exe'
+    egs.exe = os.path.join(egs.start_dir,egs.exe_name)
+    #egl_exe="$egl_pfx/drive_c/Program Files (x86)/Epic Games/Launcher/Portal/Binaries/Win32/EpicGamesLauncher.exe"
+
+    egs.game_install_dir = os.path.join(egs.wine_pfx,'drive_c/Program Files/Epic Games')
+    #egl_games_dir="$egl_pfx/drive_c/Program Files/Epic Games"
+
     ## Game Info source(s)
     # Single file containing all insalled games, but does't have the NAMES of the games :(
     #egl_game_index="$egl_pfx/drive_c/ProgramData/Epic/UnrealEngineLauncher/LauncherInstalled.dat"
@@ -32,10 +48,13 @@ def defaults():
     # Installer manifest folder (manifests are *.item files in this folder)
     # Contains ALL extensive required info for each game
     egl_game_manifests="$egl_pfx/drive_c/ProgramData/Epic/EpicGamesLauncher/Data/Manifests"
+    egs.manifest_dir = os.path.join(egs.wine_pfx,'drive_c/ProgramData/Epic/EpicGamesLauncher/Data/Manifests')
 
     #egl_game_info_ext='.mancpn'
-    egl_overlay="$egl_pfx/drive_c/Program Files (x86)/Epic Games/Launcher/Portal/Extras/Overlay/EOSOverlayRenderer-Win32-Shipping.exe"
-    egl_overlay_64="$egl_pfx/drive_c/Program Files (x86)/Epic Games/Launcher/Portal/Extras/Overlay/EOSOverlayRenderer-Win64-Shipping.exe"
+    egs.overlay = os.path.join(egs.wine_pfx,'drive_c/Program Files (x86)/Epic Games/Launcher/Portal/Extras/Overlay/EOSOverlayRenderer-Win32-Shipping.exe')
+    #egl_overlay="$egl_pfx/drive_c/Program Files (x86)/Epic Games/Launcher/Portal/Extras/Overlay/EOSOverlayRenderer-Win32-Shipping.exe"
+    egs.overlay_64 = os.path.join(egs.wine_pfx,'drive_c/Program Files (x86)/Epic Games/Launcher/Portal/Extras/Overlay/EOSOverlayRenderer-Win64-Shipping.exe')
+    #egl_overlay_64="$egl_pfx/drive_c/Program Files (x86)/Epic Games/Launcher/Portal/Extras/Overlay/EOSOverlayRenderer-Win64-Shipping.exe"
 
     # GOG
     gog_comp_dir="$compdata_dir/GogGalaxyLauncher"
@@ -71,6 +90,37 @@ def defaults():
     amz_games_dir="$amz_pfx/drive_c/Amazon Games/Library"
     amz_games_reg="$amz_pfx/user.reg"
 
+class Storefront():
+    # Storefront Name to use in shortcut
+    name = ''
+    # Path to the storefront's compatdata folder inside Steam
+    compat_path = ''
+    # Path to the root of the wine prefix in the compatdata folder
+    wine_pfx = ''
+    # The 'startdir' for the shortcut
+    start_dir = ''
+    # Name of the exe file
+    exe_name = ''
+    # Full path to the storefront's exe
+    exe = ''
+    # Launch options required by the storefront
+    # Get these from a windows generated shortcut file
+    launch_opts = ''
+    # Directory where games from the storefront are located
+    game_install_dir = ''
+
+# Epic Agmes Store specific storefront class
+class egs (Storefront):
+    # EGS stores the information about installed games in a special manifest directory that we need to be able to scan
+    # Path to the EGS store manifest directory
+    manifest_dir = ''
+    # Generally we want to advise if the EGS overlays are enabled
+    # To advise on this we need the path to the overlay exe's
+    # Path to EGS Overlay
+    overlay = ''
+    # Path to EGS 64-bit overaly
+    overlay_64 = ''
+
 class steam_paths():
     # grab the user home directory
     user_home = pathlib.Path.home()
@@ -89,7 +139,7 @@ class steam_paths():
     # The compatdata folder (i.e. proton/wine prefixes)
     global compdata_dir ; compatdata_dir = "$steamapps_dir/compatdata"
 
-def find_steam_paths():
+#def find_steam_paths():
 
 
 def generate_egl_data():
@@ -103,10 +153,10 @@ def generate_shortcut_data(name, target, start_dir, compat_dir, command_opts):
     print("launch opts: STEAM_COMPAT_DATA_PATH=\"{}\" %%command%% {}".format(compat_dir,command_opts))
 
 def main(): 
-    find_steam_paths()
+    defaults()
     print("Steam root: {}".format(steam_paths.steam_root))
     print("User home: {}".format(steam_paths.user_home))
-
+    print("EGL Name: {}".format(egs.name))
 
 #if __name__ == "__main__":
 #    main()
